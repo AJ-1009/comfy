193c2856b56da820569b3dd33eebb083972589da
ajay jakhar <gargashwin9@gmail.com>
2023-09-18 22:04:03 +0530

    my commit

--- components/common/Card.js
+++ components/common/Card.js
@@ -1,5 +1,5 @@
 
-export default function Card() {
+export default function card() {
   return (
     <div>Card</div>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from '../../components/common/Card'
+import Navbar from '../../components/common/Navbar'
+import HeroPage from '../../components/hero-page/HeroPage'
 
 export default function Page() {
   return (
     <div>
-        <Card/>
+        <HeroPage/>
     </div>
   )
 }
